d6292e382c5ad28320c85721e5e2762e2fbd673b
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-05-06 21:26:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const[search, setNewSearch] = useState('')
+  const[countriesFilter, setCountriesFilter] = useState(countries)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log(response.data[0]['name']['common'])
+        setCountries(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  // const handlesearch = (event) => {
+  //   const searchCountry = event.target.value.toLowerCase()
+  //   setNewSearch(searchCountry)
+  //   const newCountries = countries.filter (
+  //     (country) => 
+  //     country.name.toLowerCase().search(searchCountry) !== -1
+  //   )
+  //   setCountriesFilter(newCountries)
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <p>find countries: <input value={search} onChange={handlesearch}/></p> */}
+      <ul> {countries.map(each =>
+      <li>{each['name']['common']}</li>)
+        }
+       
+        {/* <li>
+          {countriesFilter.map(country =>
+            <li> country={country} </li>)}
+        </li> */}
+      </ul>
     </div>
-  );
-}
+  )
+} 
 
 export default App;
